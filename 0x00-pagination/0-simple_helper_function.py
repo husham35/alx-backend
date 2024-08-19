7eec17337ddbcf9096385050c702f0daaab1ad12
husham35 <husham35@gmail.com>
2024-08-19 00:09:12 +0000

    fixes for task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -12,6 +12,8 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     Page numbers are `1-indexed`, the first page is page 1
     Returns a pagination of data
     """
-    start = (page - 1) * page_size
-    end = start + page_size
-    return (start, end)
+    # start = (page - 1) * page_size
+    # end = start + page_size
+    # return (start, end)
+    start = page * page_size
+    return start - page_size, start
